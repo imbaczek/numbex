1ecaca73dd5ea06a7e7a4da797d0a87c9ecce7c8
Marek Baczynski <imbaczek@gmail.com>
2009-01-10 19:01:08 +0100

    bring server and client close to support the required interface

--- src/numbex_client.py
+++ src/numbex_client.py
@@ -1,33 +1,68 @@
-#
-#
-#
+from optparse import OptionParser
+import sys, time
+from datetime import datetime
 
 from NumbexServiceService_client import *
-import sys, time
 
-TRACE=None
-loc = NumbexServiceServiceLocator()
-port = loc.getNumbexServicePort(url='http://localhost:8000/', tracefile=TRACE)
-#port = loc.getNumbexServicePort(url='http://192.168.100.1:8000/echo', tracefile=TRACE)
+_outfile = sys.stdout
+_tracefile = None
+_url = 'http://localhost:8000/'
 
-msg = getData()
-msg.Value = 1
-rsp = port.getData(msg)
-print "INTEGER: ", rsp._return
+def pull_all():
+    loc = NumbexServiceServiceLocator()
+    port = loc.getNumbexServicePort(url=_url, tracefile=_tracefile)
 
-msg.Value = "HI"
-rsp = port.getData(msg)
-print "STRING: ", rsp.Value
-
-msg.Value = 1.10000
-rsp = port.getData(msg)
-print "FLOAT: ", rsp.Value
+    msg = getData()
+    rsp = port.getData(msg)
+    print "INTEGER: ", rsp._return
 
 
-msg.Value = dict(milk=dict(cost=3.15, unit="gal"))
-rsp = port.getData(msg)
-print "DICT: ", rsp.Value
+def pull(since):
+    loc = NumbexServiceServiceLocator()
+    port = loc.getNumbexServicePort(url=_url, tracefile=_tracefile)
 
-msg.Value = list('array')
-rsp = port.getData(msg)
-print "LIST:", rsp.Value
+    msg = getUpdates()
+    msg._parameter = datetime.now().timetuple()
+    rsp = port.getUpdates(msg)
+    print "pull:", rsp._return
+
+
+def send(filename):
+    loc = NumbexServiceServiceLocator()
+    port = loc.getNumbexServicePort(url=_url, tracefile=_tracefile)
+
+    msg = receiveUpdates()
+    data = file(filename).read()
+    msg._csv = data
+    rsp = port.getUpdates(msg)
+
+def main():
+    global _tracefile, _outfile
+    usage = """%prog [options] <command>\n
+Available commands:
+    pull <DATE_SINCE>\tget numer ranges modified after DATE_SINCE
+    pullall          \tget all number ranges
+    send <CSVFILE>   \tsend the contents of CSVFILE as an update"""
+    op = OptionParser(usage=usage)
+    op.add_option("-o", "--output-file", help="output file",
+            metavar="OUTFILE")
+    op.add_option("-t", "--trace-file", help="trace file (for debugging)",
+            metavar="TRACEFILE")
+    options, args = op.parse_args()
+    _tracefile = options.trace_file
+    if options.output_file:
+        _outfile = options.output_file
+    if len(args) < 1:
+        op.error("command required")
+        return
+    dispatch = {'pullall': pull_all,
+                'pull': pull,
+                'send': send}
+    try:
+        dispatch.get(args[0], lambda: op.error("unknown command"))(*args[1:])
+    except TypeError:
+        op.error("invalid parameters")
+        return
+
+if __name__ == "__main__":
+    main()
--- src/numbex_server.py
+++ src/numbex_server.py
@@ -1,6 +1,8 @@
 #!/apps/pydev/hjoukl/bin/python2.4
 # *** ZSI 2.1 ***
 from optparse import OptionParser
+from datetime import datetime
+import time
 # Import the ZSI stuff you'd need no matter what
 from ZSI.wstools import logging
 from ZSI.ServiceContainer import AsServer
@@ -17,24 +19,36 @@ class MyNumbexService(NumbexServiceService):
         # Call the generated base class method to get appropriate
         # input/output data structures
         request, response = NumbexServiceService.soap_getData(self, ps, **kw)
-        response._return = self.getSquare(1)
+        response._return = "foobar";
         return request, response
 
-    def getSquare(self, x):
-        return "1234fsadfasf"
+    def soap_getUpdates(self, ps, **kw):
+        request, response = NumbexServiceService.soap_getUpdates(self, ps, **kw)
+        since = datetime.fromtimestamp(time.mktime(request._parameter))
+        response._return = "quux"
+        return request, response
 
+    def soap_receiveUpdates(self, ps, **kw):
+        request, response = NumbexServiceService.soap_receiveUpdates(self, ps, **kw)
+        data = request._csv
+        print data
+        respons._return = True
+        return request, response
 
-op = OptionParser(usage="%prog [options]")
-op.add_option("-l", "--loglevel", help="loglevel (DEBUG, WARN)",
-metavar="LOGLEVEL")
-op.add_option("-p", "--port", help="HTTP port",
-metavar="PORT", default=8000, type="int")
-options, args = op.parse_args()
-# set the loglevel according to cmd line arg
-if options.loglevel:
-    loglevel = eval(options.loglevel, logging.__dict__)
-    logger = logging.getLogger("")
-    logger.setLevel(loglevel)
-# Run the server with a given list services
-AsServer(port=options.port, services=[MyNumbexService(),])
+def main():
+    op = OptionParser(usage="%prog [options]")
+    op.add_option("-l", "--loglevel", help="loglevel (DEBUG, WARN)",
+    metavar="LOGLEVEL")
+    op.add_option("-p", "--port", help="HTTP port",
+    metavar="PORT", default=8000, type="int")
+    options, args = op.parse_args()
+    # set the loglevel according to cmd line arg
+    if options.loglevel:
+        loglevel = eval(options.loglevel, logging.__dict__)
+        logger = logging.getLogger("")
+        logger.setLevel(loglevel)
+    # Run the server with a given list services
+    AsServer(port=options.port, services=[MyNumbexService(),])
 
+if __name__ == '__main__':
+    main()
