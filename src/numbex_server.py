3c3e8a5fc6d2c4d21e11f7dab38fde0315a9685c
Marek Baczynski <imbaczek@gmail.com>
2009-01-10 20:24:12 +0100

    use a sqlite3 database

--- src/numbex_server.py
+++ src/numbex_server.py
@@ -4,23 +4,39 @@ from optparse import OptionParser
 from datetime import datetime
 import time
 from cStringIO import StringIO
+import csv
+
 # Import the ZSI stuff you'd need no matter what
 from ZSI.wstools import logging
 from ZSI.ServiceContainer import AsServer
 
 # Import the generated Server Object
-
 from NumbexServiceService_server import NumbexServiceService
+
+# database support
+from database import Database
 
 # Create a Server implementation by inheritance
 class MyNumbexService(NumbexServiceService):
     # Make WSDL available for HTTP GET
     _wsdl = file("NumbexServiceService.wsdl").read()
+    def __init__(self, dbfile="tmp.db", *args, **kwargs):
+        NumbexServiceService.__init__(self, *args, **kwargs)
+        self.db = Database(dbfile)
+
+    def _transform_to_csv(self, data):
+        ofile = StringIO()
+        csvwriter = csv.writer(ofile)
+        for start, end, sip, date in data:
+            csvwriter.writerow([start, end, sip, date.isoformat()])
+        return ofile.getvalue()
+
     def soap_getData(self, ps, **kw):
         # Call the generated base class method to get appropriate
         # input/output data structures
         request, response = NumbexServiceService.soap_getData(self, ps, **kw)
-        response._return = "foobar"
+        data = self._transform_to_csv(self.db.get_data_all())
+        response._return = data
         return request, response
 
     def soap_getUpdates(self, ps, **kw):
@@ -32,16 +48,17 @@ class MyNumbexService(NumbexServiceService):
     def soap_receiveUpdates(self, ps, **kw):
         request, response = NumbexServiceService.soap_receiveUpdates(self, ps, **kw)
         data = request._csv
-        print data
         response._return = True
         return request, response
 
 def main():
     op = OptionParser(usage="%prog [options]")
     op.add_option("-l", "--loglevel", help="loglevel (DEBUG, WARN)",
-    metavar="LOGLEVEL")
+        metavar="LOGLEVEL")
     op.add_option("-p", "--port", help="HTTP port",
-    metavar="PORT", default=8000, type="int")
+        metavar="PORT", default=8000, type="int")
+    op.add_option("-d", "--database", help="sqlite3 database filename",
+        metavar="DBFILE", default="tmp.db")
     options, args = op.parse_args()
     # set the loglevel according to cmd line arg
     if options.loglevel:
@@ -49,7 +66,7 @@ def main():
         logger = logging.getLogger("")
         logger.setLevel(loglevel)
     # Run the server with a given list services
-    AsServer(port=options.port, services=[MyNumbexService(),])
+    AsServer(port=options.port, services=[MyNumbexService(options.database),])
 
 if __name__ == '__main__':
     main()
