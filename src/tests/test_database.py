3a1a20dae19da7c143a8cfe7aa6aee8ec32a8950
Marek Baczynski <imbaczek@gmail.com>
2009-02-04 16:59:19 +0100

    nowe testy dla database

--- src/tests/test_database.py
+++ src/tests/test_database.py
@@ -20,5 +20,41 @@ class TestDatabase(unittest.TestCase):
         self.assertEqual(v, r)
 
 
+class TestDBInsertOverlap(unittest.TestCase):
+    def setUp(self):
+        self.db = database.Database(':memory:', fill_example=False)
+        self.db.create_db()
+        v1 = (u'+48581234', u'+48581999', u'sip.freeconet.pl',
+            u'freeconet', datetime.datetime.now(), u'')
+        c = self.db.conn.cursor()
+        self.db.insert_range(c, *v1)
+        c.close()
+        self.cursor = self.db.conn.cursor()
+
+    def tearDown(self):
+        self.cursor.close()
+         
+    def test_overlap_inner(self):
+        v2 = (u'+48581300', u'+48581500', u'sip.freeconet.pl',
+            u'freeconet', datetime.datetime.now(), u'')
+        self.assertRaises(ValueError, self.db.insert_range, self.cursor, *v2)
+
+    def test_overlap_outer(self):
+        v2 = (u'+48581200', u'+48582500', u'sip.freeconet.pl',
+            u'freeconet', datetime.datetime.now(), u'')
+        self.assertRaises(ValueError, self.db.insert_range, self.cursor, *v2)
+
+    def test_overlap_left(self):
+        v2 = (u'+48581200', u'+48581300', u'sip.freeconet.pl',
+            u'freeconet', datetime.datetime.now(), u'')
+        self.assertRaises(ValueError, self.db.insert_range, self.cursor, *v2)
+
+    def test_overlap_right(self):
+        v2 = (u'+48581900', u'+48582500', u'sip.freeconet.pl',
+            u'freeconet', datetime.datetime.now(), u'')
+        self.assertRaises(ValueError, self.db.insert_range, self.cursor, *v2)
+
+
+
 if __name__ == '__main__':
     unittest.main()
