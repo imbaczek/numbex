48123684ebb1db3d90fd3cad6b6c49f384b9c7df
Marek Baczynski <imbaczek@gmail.com>
2009-02-04 16:43:40 +0100

    dodanie testu na rownosc sign_csv_record i sign_record

--- src/tests/test_crypto.py
+++ src/tests/test_crypto.py
@@ -18,6 +18,13 @@ class TestSignatures(unittest.TestCase):
         sig = crypto.sign_record(self.dsa, *v)
         self.assert_(crypto.check_signature(dsapub, sig, *v))
 
+    def test_csv_signature(self):
+        v = ['+48581234', '+48581999', 'sip.freeconet.pl', 'freeconet', datetime.datetime.now()]
+        v1 = crypto.make_csv_record(*v)
+        s = crypto.sign_record(self.dsa, *v)
+        self.assert_(crypto.check_signature(self.dsa, s, *v))
+        s1 = crypto.sign_csv_record(self.dsa, v1)
+        self.assert_(crypto.check_csv_signature(self.dsa, s1, v1))
 
 
 if __name__ == '__main__':
